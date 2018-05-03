ef55725557452c217a7d491465d0176389a0002f
gongwenbo <1071259226@qq.com>
2018-05-03 20:18:53 +0800

    颜色

--- app/src/main/java/com/gwb/superrecycleview/adapter/GoodsPropertyAdapter.java
+++ app/src/main/java/com/gwb/superrecycleview/adapter/GoodsPropertyAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,6 +20,10 @@ import java.util.List;
  */
 
 public class GoodsPropertyAdapter extends RecyclerView.Adapter<BaseViewHolder> {
+
+    private final String COLOR_SELECT = "#ffffff";
+    private final String COLOR_EMPTY  = "#BBBBBB";
+    private final String COLOR_NORMAL = "#6D6D6D";
 
     private List<GoodsPropertyBean.AttributesBean> mAttributes;
     private List<GoodsPropertyBean.StockGoodsBean> mStockGoods;
@@ -49,16 +54,23 @@ public class GoodsPropertyAdapter extends RecyclerView.Adapter<BaseViewHolder> {
         FlowLayout flowLayout = holder.getView(R.id.flowLayout);
         List<String> attributesItem = attributesBean.getAttributesItem();
         for (int j = 0; j < attributesItem.size(); j++) {
-            String property = attributesItem.get(j);
+            final String property = attributesItem.get(j);
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             TextView tv = new TextView(mContext);
-            lp.setMargins(5, 5, 5, 5);
-            tv.setPadding(10, 10, 10, 10);
-            tv.setBackgroundColor(Color.GRAY);
+            lp.setMargins(10, 10, 10, 10);
+            tv.setPadding(40, 20, 40, 20);
+            tv.setBackgroundResource(R.drawable.normal);
+            tv.setTextColor(Color.parseColor(COLOR_EMPTY));
             tv.setLayoutParams(lp);
             tv.setText(property);
             // 添加
             flowLayout.addView(tv);
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         }
     }
 
