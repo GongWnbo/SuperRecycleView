bc96950b10ca4f20d4b5785fb37eccf7913ab160
gongwenbo <1071259226@qq.com>
2018-05-18 14:01:13 +0800

    购物车数量动画

---
+++ app/src/main/java/com/gwb/superrecycleview/entity/ShopGoodsBean.java
@@ -0,0 +1,21 @@
+package com.gwb.superrecycleview.entity;
+
+/**
+ * Created by ${GongWenbo} on 2018/5/18 0018.
+ */
+public class ShopGoodsBean {
+
+    private int count;
+
+    public ShopGoodsBean(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
--- app/src/main/java/com/gwb/superrecycleview/ui/activity/ShopGoodsActivity.java
+++ app/src/main/java/com/gwb/superrecycleview/ui/activity/ShopGoodsActivity.java
@@ -1,14 +1,23 @@
 package com.gwb.superrecycleview.ui.activity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.ArrayMap;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gwb.superrecycleview.R;
 import com.gwb.superrecycleview.adapter.BaseAdapter;
 import com.gwb.superrecycleview.adapter.BaseViewHolder;
+import com.gwb.superrecycleview.entity.ShopGoodsBean;
+import com.gwb.superrecycleview.utils.ToastUtil;
 import com.gwb.superrecycleview.utils.Util;
 
 import java.util.ArrayList;
@@ -17,11 +26,14 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ShopGoodsActivity extends AppCompatActivity implements BaseAdapter.BaseAdapterListener<String> {
+public class ShopGoodsActivity extends AppCompatActivity implements BaseAdapter.BaseAdapterListener<ShopGoodsBean> {
 
     @BindView(R.id.rv_goods)
     RecyclerView mRvGoods;
-    private List<String> mGoodsList = new ArrayList<>();
+    int reduceLeft = 0;
+    int addLeft    = 0;
+    private List<ShopGoodsBean> mGoodsList = new ArrayList<>();
+    //    private SparseArray<Integer> mSparseArrayCount = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +45,8 @@ public class ShopGoodsActivity extends AppCompatActivity implements BaseAdapter.
     }
 
     private void initData() {
-        for (int i = 0; i < 4; i++) {
-            mGoodsList.add("");
+        for (int i = 0; i < 10; i++) {
+            mGoodsList.add(new ShopGoodsBean(i));
         }
     }
 
@@ -45,9 +57,84 @@ public class ShopGoodsActivity extends AppCompatActivity implements BaseAdapter.
     }
 
     @Override
-    public void convert(BaseViewHolder holder, String str) {
+    public void convert(final BaseViewHolder holder, ShopGoodsBean bean) {
         // 原价
         TextView tv_goods_original_price = holder.getView(R.id.tv_goods_original_price);
         Util.drawStrikethrough(tv_goods_original_price);
+        // TODO: 2018/5/18 0018 动画效果
+        // 商品数
+        final TextView tv_goods_count = holder.getView(R.id.tv_goods_count);
+        // 减少
+        final ImageView iv_goods_reduce = holder.getView(R.id.iv_goods_reduce);
+        iv_goods_reduce.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                reduceLeft = iv_goods_reduce.getLeft();
+                iv_goods_reduce.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+        iv_goods_reduce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getLayoutPosition();
+                ShopGoodsBean shopGoodsBean = mGoodsList.get(position);
+                int count = shopGoodsBean.getCount();
+                count--;
+                if (count == 0) {
+                    animClose(iv_goods_reduce);
+                    tv_goods_count.setText("");
+                } else {
+                    tv_goods_count.setText(String.valueOf(count));
+                }
+                shopGoodsBean.setCount(count);
+                ToastUtil.showToast(ShopGoodsActivity.this, "减少");
+            }
+        });
+        // 增加
+        final ImageView iv_goods_add = holder.getView(R.id.iv_goods_add);
+        iv_goods_add.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                addLeft = iv_goods_add.getLeft();
+                iv_goods_add.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+        iv_goods_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getLayoutPosition();
+                ShopGoodsBean shopGoodsBean = mGoodsList.get(position);
+                int count = shopGoodsBean.getCount();
+                count++;
+                if (count == 1) {
+                    iv_goods_reduce.setVisibility(View.VISIBLE);
+                    animOpen(iv_goods_reduce);
+                    ToastUtil.showToast(ShopGoodsActivity.this, "增加");
+                }
+                tv_goods_count.setText(String.valueOf(count));
+                shopGoodsBean.setCount(count);
+            }
+        });
+        int count = bean.getCount();
+        tv_goods_count.setText(count == 0 ? "" : String.valueOf(count));
+        iv_goods_reduce.setVisibility(count == 0 ? View.INVISIBLE : View.VISIBLE);
+    }
+
+    public void animOpen(ImageView imageView) {
+        ObjectAnimator oa = ObjectAnimator.ofFloat(imageView, "translationX", addLeft - reduceLeft, 0);
+        oa.setDuration(300);
+        oa.start();
+        ObjectAnimator oa1 = ObjectAnimator.ofFloat(imageView, "rotation", 0, 180);
+        oa1.setDuration(300);
+        oa1.start();
+    }
+
+    public void animClose(ImageView imageView) {
+        ObjectAnimator oa = ObjectAnimator.ofFloat(imageView, "translationX", 0, addLeft - reduceLeft);
+        oa.setDuration(300);
+        oa.start();
+        ObjectAnimator oa1 = ObjectAnimator.ofFloat(imageView, "rotation", 0, 180);
+        oa1.setDuration(300);
+        oa1.start();
     }
 }
